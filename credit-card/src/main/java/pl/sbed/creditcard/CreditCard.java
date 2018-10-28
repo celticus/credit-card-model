61dc5c627e42c71009682b5bf0c54edec14178ed
Sebastian Bednarczyk <sbednarczyk92@gmail.com>
2018-10-28 12:35:17 +0000

    cos

--- credit-card/src/main/java/pl/sbed/creditcard/CreditCard.java
+++ credit-card/src/main/java/pl/sbed/creditcard/CreditCard.java
@@ -2,7 +2,6 @@ package pl.sbed.creditcard;
 //import org.junit.Assert;
 //import org.junit.Test;
 class CreditCard {
-   /* It shows current account balance */
     public double getBalance()
     {
         return 2000;
